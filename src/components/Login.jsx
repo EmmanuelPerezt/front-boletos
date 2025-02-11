938ed9f3158ec4589265d3f3b8f6565d51240f0f
EmmanuelPerezt <emmanuel.pereztj@gmail.com>
2025-02-10 22:18:13 -0600

    add: implement Login component with modal and form for user authentication

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,102 @@
 import React, { useEffect } from "react";
 
 //todo hacer login
-export const login = ()=>{
+export const Login = () => {
+  return (
+    <>
+      <div>
+        <button
+          type="button"
+          className="btn btn-light"
+          data-bs-toggle="modal"
+          data-bs-target="#exampleModal"
+        >
+          Iniciar Sesión
+        </button>
+      </div>
 
-
-    useEffect(()=>console.log("hola"))
-
+      <div
+        className="modal fade"
+        id="exampleModal"
+        tabIndex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h1
+                className="modal-title fs-5"
+                id="exampleModalLabel"
+                style={{ color: "white" }}
+              >
+                Iniciar sesión
+              </h1>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="modal-body">
+              <form>
+                <div className="mb-3">
+                  <label
+                    htmlFor="exampleInputEmail1"
+                    className="form-label"
+                    style={{ color: "white" }}
+                  >
+                    Username
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="exampleInputEmail1"
+                    aria-describedby="emailHelp"
+                  />
+                  <div id="emailHelp" className="form-text">
+                    Nunca lo compartas con nadie
+                  </div>
+                </div>
+                <div className="mb-3">
+                  <label
+                    htmlFor="exampleInputPassword1"
+                    className="form-label"
+                    style={{ color: "white" }}
+                  >
+                    Password
+                  </label>
+                  <input
+                    type="password"
+                    className="form-control"
+                    id="exampleInputPassword1"
+                  />
+                </div>
+                <div className="mb-3 form-check">
+                  <input
+                    type="checkbox"
+                    className="form-check-input"
+                    id="exampleCheck1"
+                  />
+                  <label className="form-check-label" htmlFor="exampleCheck1">
+                    Check me out
+                  </label>
+                </div>
+                <button type="submit" className="btn btn-light">
+                  Iniciar Sesión
+                </button>
+                <button type="button" className="btn btn-dark">
+                  Registrarse
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+const requestLogin = (username,password)=> {
+    //to do hacer la request al login y setear el local storage
 }
